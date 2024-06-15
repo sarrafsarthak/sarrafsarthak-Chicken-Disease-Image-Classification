07afd595068a7459cfd392e86c83a421bd5efecb
sarrafsarthak <s.sarthak.05@gmail.com>
2024-06-15 07:48:03 +0530

    folder structure added

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.md", "r", encoding = "utf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
@@ -11,16 +11,19 @@ AUTHOR_USER_NAME = "sarrafsarthak"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "s.sarthak.05@gmail.com"
 
+
 setuptools.setup(
-    name = SRC_REPO,
-    version = __version__,
-    author = AUTHOR_USER_NAME,
-    author_email = AUTHOR_EMAIL,
-    description = "A small python package for CNN app",
-    Long_description = long_description,
-    Long_description_content = "text/markdown",
-    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls = {"Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"},
-    package_dir = {"": "src"},
-    packages = setuptools.find_packages(where = "src")
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
--- template.py
+++ template.py
@@ -2,20 +2,21 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format = '[%(asctime)s]: %(message)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
 
 project_name = "cnnClassifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
-    f"src/{project_name}/_init_.py",
-    f"src/{project_name}/component/_init_.py",
-    f"src/{project_name}/utils/_init_.py",
-    f"src/{project_name}/config/_init_.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/_init_.py",
-    f"src/{project_name}/entity/_init_.py",
-    f"src/{project_name}/constants/_init_.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
@@ -23,6 +24,8 @@ list_of_files = [
     "setup.py",
     "research/trials.ipynb",
     "templates/index.html"
+
+
 ]
 
 
@@ -30,14 +33,16 @@ for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-    if filedir != "":
-        os.makedirs(filedir, exist_ok = True)
-        logging.info(f"Creating directory; {filedir} for the file : {filename}")
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
+
 
     else:
         logging.info(f"{filename} is already exists")
