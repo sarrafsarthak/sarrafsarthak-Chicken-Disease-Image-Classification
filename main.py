45f3e3a6d3bfdbffe80035f587f8b597cd3a7870
sarrafsarthak <s.sarthak.05@gmail.com>
2024-06-24 09:50:45 +0530

    dvc added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -41,3 +42,14 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Evaluation Stage"
+try:
+   logger.info(f"***********************")
+   logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+   model_evaluation = EvaluationPipeline()
+   model_evaluation.main()
+   logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==================x")
+except Exception as e:
+   logger.exception(e)
+   raise e
