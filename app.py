e01aee38e8309bc79131161dacac89f1658ec071
sarrafsarthak <s.sarthak.05@gmail.com>
2024-06-26 20:43:43 +0530

    updated app

--- app.py
+++ app.py
@@ -4,29 +4,35 @@ from flask_cors import CORS, cross_origin
 from cnnClassifier.utils.common import decodeImage
 from cnnClassifier.pipeline.predict import PredictionPipeline
 
+
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
 app = Flask(__name__)
 CORS(app)
 
+
 class ClientApp:
     def __init__(self):
         self.filename = "inputImage.jpg"
         self.classifier = PredictionPipeline(self.filename)
 
-@app.route("/", methods = ["GET"])
+
+@app.route("/", methods=['GET'])
 @cross_origin()
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/train", methods = ["GET","POST"])
+
+@app.route("/train", methods=['GET','POST'])
 @cross_origin()
 def trainRoute():
-    os.system("Python main.py")
-    return "Training done Successfully!"
+    os.system("python main.py")
+    return "Training done successfully!"
 
-@app.route("/predict", methods = ["POST"])
+
+
+@app.route("/predict", methods=['POST'])
 @cross_origin()
 def predictRoute():
     image = request.json['image']
